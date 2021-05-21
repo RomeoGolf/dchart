f5fb06892d2e2098c96392fa27d246dfb247c024
RomeoGolf <romeogolf@mail.ru>
2021-05-21 15:01:00 +0500

    refactor: Redesign the test window

--- main.cpp
+++ main.cpp
@@ -10,15 +10,11 @@ int main (int argc, char ** argv)
   Fl_Window *window;
   Fl_Box *box;
 
-  window = new Fl_Window (300, 300);
-  box = new Fl_Box (10, 10, 150, 30, "Hello World!");
+  window = new Fl_Window (1000, 800);
 
-  box->box (FL_UP_BOX);
-  box->labelsize (21);
-  box->labelfont (FL_BOLD+FL_ITALIC);
-  box->labeltype (FL_SHADOW_LABEL);
-
-  DChartBase *dcb = new DChartBase(10, 50, 200, 200, "");
+  DChartBase *dcb = new DChartBase(20, 20,
+                                   window->w() - 40,
+                                   window->h() - 40, "");
   dcb->labelsize(21);
   window->resizable(dcb);
 
