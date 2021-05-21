24e335a312f90b11963836c3c0bede3d053902ba
RomeoGolf <romeogolf@mail.ru>
2021-05-21 09:34:37 +0500

    feat: Add a draft for a basic axis class

---
+++ include/BasicAxis.h
@@ -0,0 +1,16 @@
+#ifndef BASICAXIS_H
+#define BASICAXIS_H
+
+
+class BasicAxis
+{
+    public:
+        BasicAxis();
+        virtual ~BasicAxis();
+
+    protected:
+
+    private:
+};
+
+#endif // BASICAXIS_H
---
+++ src/BasicAxis.cpp
@@ -0,0 +1,11 @@
+#include "BasicAxis.h"
+
+BasicAxis::BasicAxis()
+{
+    //ctor
+}
+
+BasicAxis::~BasicAxis()
+{
+    //dtor
+}
