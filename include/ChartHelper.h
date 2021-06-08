2813c27e0427f911525fed670639d440813634ae
RomeoGolf <romeogolf@mail.ru>
2021-06-08 10:16:56 +0500

    style: Format a code fragment

--- include/ChartHelper.h
+++ include/ChartHelper.h
@@ -14,9 +14,10 @@ class ChartHelper
         ChartHelper();
         virtual ~ChartHelper();
 
-        static constexpr Fl_Color colors[] = {FL_RED, FL_GREEN, FL_BLUE, FL_MAGENTA, FL_BLACK,
-        FL_DARK_RED, FL_DARK_GREEN, 176, FL_DARK_CYAN,  FL_DARK_MAGENTA,
-        FL_DARK_YELLOW, 181, 91, 243, 235, 162, 115, 200, 212, 83, FL_CYAN, FL_YELLOW};
+        static constexpr Fl_Color colors[] = {FL_RED, FL_GREEN, FL_BLUE,
+            FL_MAGENTA, FL_BLACK, FL_DARK_RED, FL_DARK_GREEN, 176, FL_DARK_CYAN,
+            FL_DARK_MAGENTA, FL_DARK_YELLOW, 181, 91, 243, 235, 162, 115, 200,
+            212, 83, FL_CYAN, FL_YELLOW};
 
         // margins from a widget border to a chart area
         int marginLeft;
